e4bd970592e9a85cee5394770f8a858da0a55470
eoudejans <eoudejans@objectvision.nl>
2026-07-13 12:57:58 +0200

    Added tif conversion script. Updated .gitignore

---
+++ data/convert_tifs.bat
@@ -0,0 +1,4 @@
+python ./convert-tif-to-cog.py ./tif/bomen_3_rule_2026.tif --colors "#EBE4B1,#54B6C0"
+python ./convert-tif-to-cog.py ./tif/bomen_30_rule_2026.tif --colors "#EBE4B1,#54B6C0"
+python ./convert-tif-to-cog.py ./tif/bomen_300_rule_2026.tif --colors "#EBE4B1,#54B6C0"
+python ./convert-tif-to-cog.py ./tif/bomen_3_30_300_rule_2026.tif --colors "#DADADA,#F1DD9B,#E9E4A9,#B1D2A5,#96C9B0,#71B8BE,#4496B3,#1E6299"
